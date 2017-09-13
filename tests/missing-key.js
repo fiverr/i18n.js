821f33c3f0af0b010e7e68d0bd84c51899ce4f8e
Omri Lotan <omri@hamadgera.com>
2017-09-13 18:00:22 +0300

    Add singleton to interface

--- tests/missing-key.js
+++ tests/missing-key.js
@@ -2,20 +2,18 @@ const {expect} = require('chai');
 
 const I18n = require('../');
 
-module.exports = () => {
-    describe('missing keys report', () => {
-        it('reports missing keys', () => {
-            const i18n = new I18n({
-                $scope: 'some.scope',
-                missing: (key, scope, translations) => {
-                    expect(scope).to.equal('some.scope');
-                    expect(key).to.equal('a.missing.key');
-                    expect(translations).to.be.an('object');
-                    expect(translations).to.be.empty;
-                }
-            });
-
-            i18n.translate('a.missing.key');
+describe('missing keys report', () => {
+    it('reports missing keys', () => {
+        const i18n = new I18n({
+            $scope: 'some.scope',
+            missing: (key, scope, translations) => {
+                expect(scope).to.equal('some.scope');
+                expect(key).to.equal('a.missing.key');
+                expect(translations).to.be.an('object');
+                expect(translations).to.be.empty;
+            }
         });
+
+        i18n.translate('a.missing.key');
     });
-};
+});
