8a4b0d8e70719fa0532cd92393cbe81392111d8d
Omri Lotan <omrilotan@users.noreply.github.com>
2019-12-18 11:37:10 +0200

    👕

--- tests/missing-key.js
+++ tests/missing-key.js
@@ -42,7 +42,7 @@ describe('missing keys report', () => {
         undefined,
         null
     ].forEach(
-        item => it(`reports empty value for ${item}`, () => {
+        (item) => it(`reports empty value for ${item}`, () => {
             let reported = false;
             const i18n = new I18n({
                 $scope: 'some.scope',
