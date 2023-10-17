f7aefe14c04d2ceb38483c38475ec2d78bc14801
Illia Savin <106974587+ilsavin-fiverr@users.noreply.github.com>
2023-10-17 17:43:29 +0300

    Revert "[FEAT] templates support added (#87)"
    This reverts commit 2810e3aacfb5a0520033885c8ff06b5c561fdb8c.

--- utils/get-one-other.js
+++ utils/get-one-other.js
@@ -1,5 +1,3 @@
-const { has } = require('lodash');
-
 /**
  * get "one" or "other" from translation key item
  * @param  {Any} result
@@ -21,6 +19,16 @@ module.exports = function getOneOther(result, data) {
  * @return {Boolean} The conditions meet a one/other use case
  */
 const isOneOther = (result, data) =>
+    typeof result === 'object' &&
+    typeof data === 'object' &&
     has(result, 'one') &&
     has(result, 'other') &&
     has(data, 'count');
+
+/**
+ * Use Object prototype's hasOwnProperty directly
+ * @param  {Object} target
+ * @param  {String} property
+ * @return {Boolean}
+ */
+const has = (target, property) => Object.prototype.hasOwnProperty.call(target, property);
